82dd604138e4d07534142c1e0481fc7ff5f253cf
Vadim_Nesterov <alan@xaker.ru>
2021-12-02 22:37:53 +0300

    Day 2

---
+++ src/Day02.kt
@@ -0,0 +1,58 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var depth = 0
+        var position = 0
+
+        for (line in input) {
+            val (command, number) = line.split(" ")
+
+            when (command) {
+                "forward" -> position += number.toInt()
+                "down" -> depth += number.toInt()
+                "up" -> depth -= number.toInt()
+            }
+        }
+
+        return depth * position
+    }
+
+    fun part2(input: List<String>): Int {
+        var depth = 0
+        var position = 0
+        var aim = 0
+
+        for (line in input) {
+            val (command, numberStr) = line.split(" ")
+            val number = numberStr.toInt()
+
+            when (command) {
+                "forward" -> {
+                    position += number
+                    depth += aim * number
+                }
+                "down" -> aim += number
+                "up" -> aim -= number
+            }
+        }
+
+        return depth * position
+    }
+
+    val testInput = readInput("Day02_test")
+
+    val result1 = part1(testInput)
+    assert(150, result1)
+
+    val result2 = part2(testInput)
+    assert(900, result2)
+
+    val input = readInput("Day02")
+
+    val actual1 = part1(input)
+    println(actual1)
+    assert(1507611, actual1)
+
+    val actual2 = part2(input)
+    println(actual2)
+    assert(1880593125, actual2)
+}
