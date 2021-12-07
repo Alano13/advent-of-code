5a2c918a7d25b65ee6baff6748085cbe40ec7a66
Vadim_Nesterov <alan@xaker.ru>
2021-12-07 23:02:14 +0300

    Day 7

---
+++ src/day07/Day07.kt
@@ -0,0 +1,61 @@
+package day07
+
+import runAssert
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val positions = input[0]
+            .split(',')
+            .map { it.toInt() }
+
+        val max = positions.maxOf { it }
+        val min = positions.minOf { it }
+
+        return (min..max)
+            .toList()
+            .minOf { optimalPositionToCheck ->
+                positions.sumOf { position ->
+                    abs(position - optimalPositionToCheck)
+                }
+            }
+    }
+
+    fun part2(input: List<String>): Int {
+        val positions = input[0]
+            .split(',')
+            .map { it.toInt() }
+
+        val max = positions.maxOf { it }
+        val min = positions.minOf { it }
+
+        return (min..max)
+            .toList()
+            .minOf { optimalPositionToCheck ->
+                positions.sumOf { position ->
+                    (0..abs(position - optimalPositionToCheck))
+                        .toList()
+                        .sum()
+                }
+            }
+    }
+
+    println("Part 1:")
+    runAssert(
+        7,
+        37,
+        355150,
+        ::part1
+    )
+
+    println()
+    println("Part 2:")
+
+    runAssert(
+        7,
+        168,
+        98368490,
+        ::part2
+    )
+}
+
